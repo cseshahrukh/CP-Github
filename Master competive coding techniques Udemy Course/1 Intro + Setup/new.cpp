8826acca2088b5c0b1b342d22ecaf8e0d05c89b7
cse <cseshahrukh@gmail.com>
2024-09-28 21:19:36 +0600

    idea from udemy cp

---
+++ Master competive coding techniques Udemy Course/1 Intro + Setup/new.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define int long long int
+#define F first 
+#define S second
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define MOD 1000000007
+#define MAX 1e9
+#define MIN -1e9
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+
+
+#define endl "\n"
+using namespace std; 
+
+int32_t main() {
+//Won't work in hackerrank, hackerearth    
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    cout << "Hello World!" << endl;
+
+    int n; 
+    cin>>n; 
+    cout<<n<<endl; 
+    return 0;
+
+}
